7893dfc8a2386014abebe5cf1dbfd3497ca12903
Ashish Nevan <nevchris242@gmail.com>
2025-05-27 10:23:01 -0400

    tests and auth

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from assignment1!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
+from src.api import router
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
